edad3fa51b5206462abf389657daf3a771a8d9da
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-12-23 10:43:58 -0600

    Animation Added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let euro = document.querySelector("#euro");
 let pound = document.querySelector("#pound");
 let peso = document.querySelector("#peso");
+let results = document.querySelector("#results");
 
 let input = document.querySelector("#input");
 input.addEventListener("input", inputListener);
@@ -12,6 +13,12 @@ function inputListener(e) {
   euro.innerText = value * 0.88;
   pound.innerText = value * 0.79;
   peso.innerText = value * 19.94;
+
+  if (value !== "") {
+    results.style.display = "block";
+  } else {
+    return;
+  }
 }
 
 console.log(euro.innerText);
